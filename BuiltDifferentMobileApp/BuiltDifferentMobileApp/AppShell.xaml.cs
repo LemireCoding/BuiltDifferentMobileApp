4e155c55b794b7f4804a698dfc9030bf87522f56
olivierLemire <olivierlemire@urbanimmersive.com>
2021-11-29 14:37:50 -0500

    Add project files.

---
+++ BuiltDifferentMobileApp/BuiltDifferentMobileApp/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using BuiltDifferentMobileApp.ViewModels;
+using BuiltDifferentMobileApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace BuiltDifferentMobileApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ BuiltDifferentMobileApp/BuiltDifferentMobileApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using BuiltDifferentMobileApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace BuiltDifferentMobileApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
