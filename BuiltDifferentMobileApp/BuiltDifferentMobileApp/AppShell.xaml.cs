0b775515d1abd99076313dd3e128b141198f539b
Brandon Thorng <Dragonspear999@gmail.com>
2021-11-29 22:16:48 -0500

    Fixed project

---
+++ BuiltDifferentMobileApp/BuiltDifferentMobileApp/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using BuiltDifferentMobileApp.ViewModels;
+using BuiltDifferentMobileApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace BuiltDifferentMobileApp {
+    public partial class AppShell : Xamarin.Forms.Shell {
+        public AppShell() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BuiltDifferentMobileApp/BuiltDifferentMobileApp/Services/NetworkServices/INetworkService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace BuiltDifferentMobileApp.Services.NetworkServices {
+    public interface INetworkService<T> where T : HttpResponseMessage, new() {
+    }
+}
