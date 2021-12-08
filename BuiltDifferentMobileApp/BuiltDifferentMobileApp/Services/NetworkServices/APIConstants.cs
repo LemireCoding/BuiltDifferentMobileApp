66b03059abd0ec28142d40ac0c2813aacaea8473
Brandon Thorng <Dragonspear999@gmail.com>
2021-12-08 02:53:42 -0500

    Fixed mobile device platform boolean evaluating incorrectly

--- BuiltDifferentMobileApp/BuiltDifferentMobileApp/Services/NetworkServices/APIConstants.cs
+++ BuiltDifferentMobileApp/BuiltDifferentMobileApp/Services/NetworkServices/APIConstants.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Essentials;
+using Xamarin.Forms;
 
 namespace BuiltDifferentMobileApp.Services.NetworkServices
 {
     public static class APIConstants
     {
-        public static string BaseAddress = DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:8080" : "http://localhost:8080";
+        public static string BaseAddress = Device.RuntimePlatform == Device.Android ? "http://10.0.2.2:8080" : "http://localhost:8080";
 
         public static string PostWorkoutUri()
         {
