46232859f2fbb037f55056e2f1e8152f790da254
Chloedunwoody <chloe.dunwoody@hotmail.com>
2021-12-09 22:10:12 -0500

    Test enviro up

---
+++ BuiltDifferent.UITest/AppInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace BuiltDifferent.UITest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .InstalledApp("com.companyname.builtdifferentmobileapp")
+                    .EnableLocalScreenshots()
+                    .Debug()
+                    .StartApp();
+            }
+
+            return ConfigureApp
+                .iOS
+                .StartApp();
+        }
+    }
+}
