28f9911494f5153d8ec8e37461988be4835c24b1
Brandon Thorng <brandonthorng@gmail.com>
2021-12-22 15:30:50 -0500

    Implemented search bar for client filtering

--- BuiltDifferentMobileApp/BuiltDifferentMobileApp/ViewModels/Coach/CoachDashboardPageViewModel.cs
+++ BuiltDifferentMobileApp/BuiltDifferentMobileApp/ViewModels/Coach/CoachDashboardPageViewModel.cs
@@ -3,6 +3,7 @@ using BuiltDifferentMobileApp.Services.NetworkServices;
 using BuiltDifferentMobileApp.Views.Coach;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,7 +18,17 @@ namespace BuiltDifferentMobileApp.ViewModels.Coach {
 
         private int coachId;
 
+        private List<Models.Client> originalClients { get; set; }
         public ObservableRangeCollection<Models.Client> Clients { get; set; }
+
+        private string searchTerm;
+        public string SearchTerm {
+            get => searchTerm;
+            set {
+                SetProperty(ref searchTerm, value);
+                FilterResults();
+            }
+        }
 
         public AsyncCommand ViewMyProfileCommand { get; }
         public AsyncCommand RefreshCommand { get; }
@@ -35,6 +46,17 @@ namespace BuiltDifferentMobileApp.ViewModels.Coach {
             FetchClients();
         }
 
+        private void FilterResults() {
+            if(originalClients == null) return;
+
+            if(string.IsNullOrWhiteSpace(SearchTerm)) {
+                Clients.ReplaceRange(originalClients);
+                return;
+            }
+
+            Clients.ReplaceRange(originalClients.Where(c => c.name.ToLower().Contains(SearchTerm.ToLower().Trim())));
+        }
+
         private async Task ViewClientsBoard(Models.Client client) {
             if(client == null || IsBusy) return;
 
@@ -47,10 +69,14 @@ namespace BuiltDifferentMobileApp.ViewModels.Coach {
             List<Models.Client> clients = await networkService.GetAsync<List<Models.Client>>(APIConstants.GetClientsForCoachId(coachId));
 
             if(clients.Count != 0) {
+                originalClients = clients;
                 Clients = new ObservableRangeCollection<Models.Client>(clients);
             } else {
+                originalClients = null;
                 Clients = new ObservableRangeCollection<Models.Client>();
             }
+
+            FilterResults();
 
             OnPropertyChanged("Clients");
             IsBusy = false;
