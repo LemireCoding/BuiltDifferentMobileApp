60ed7315667d72deab5f46d8961d8269edf975d5
Chloedunwoody <chloe.dunwoody@hotmail.com>
2022-01-14 03:49:23 -0500

    t

--- BuiltDifferentMobileApp/BuiltDifferentMobileApp/ViewModels/Profile/MyProfilePageCoachViewModel.cs
+++ BuiltDifferentMobileApp/BuiltDifferentMobileApp/ViewModels/Profile/MyProfilePageCoachViewModel.cs
@@ -3,12 +3,25 @@ using BuiltDifferentMobileApp.Services.AccountServices;
 using BuiltDifferentMobileApp.Services.NetworkServices;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using Xamarin.CommunityToolkit.ObjectModel;
 
 namespace BuiltDifferentMobileApp.ViewModels.Profile {
     public class MyProfilePageCoachViewModel : ViewModelBase {
 
-        public string Name { get; set; }
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                SetProperty(ref name, value);
+                OnPropertyChanged(nameof(IsEnabled));
+
+            }
+        }
+
         public string Type { get; set; }
         public bool IsAvailable { get; set; }
         public bool OffersMeal { get; set; }
@@ -17,7 +30,20 @@ namespace BuiltDifferentMobileApp.ViewModels.Profile {
         public string Gender { get; set; }
         public bool IsVerified { get; set; }
 
+        private bool isEnabled;
+        public bool IsEnabled
+        {
+            get => isEnabled;
+            set => SetProperty(ref isEnabled, value);
+        }
+        private int UserId;
+        private int Id;
+
+        public AsyncCommand SubmitCommand { get; }
+        public AsyncCommand EditProfileCommand { get; }
+
         private IAccountService accountService = AccountService.Instance;
+        private INetworkService<HttpResponseMessage> networkService = NetworkService<HttpResponseMessage>.Instance;
 
         public MyProfilePageCoachViewModel() {
             Models.Coach user = (Models.Coach)accountService.CurrentUser;
@@ -30,6 +56,7 @@ namespace BuiltDifferentMobileApp.ViewModels.Profile {
             CertificationId = user.certificationId;
             Gender = user.gender;
             IsVerified = user.isVerified;
+            IsEnabled = false;
         }
 
     }
