4a2507c98d72e6b02a96e441a4a09510cdd65ad3
Chloedunwoody <chloe.dunwoody@hotmail.com>
2022-01-18 22:40:05 -0500

    Set current user and get API route

---
+++ BuiltDifferentMobileApp/BuiltDifferentMobileApp/Models/ClientProfileRecieveDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuiltDifferentMobileApp.Models
+{
+    public class ClientProfileRecieveDTO
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int userId { get; set; }
+        public int coachId { get; set; }
+        public bool waitingApproval { get; set; }
+        public int startWeight { get; set; }
+        public int currentWeight { get; set; }
+        public string profilePicture { get; set; }
+    }
+}
