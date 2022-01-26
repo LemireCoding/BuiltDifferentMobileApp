976ad6183d605e0840ab6e28b78c88648a761746
Brandon Thorng <brandonthorng@gmail.com>
2022-01-25 20:18:28 -0500

    Added account status models

---
+++ BuiltDifferentMobileApp/BuiltDifferentMobileApp/Models/AccountStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuiltDifferentMobileApp.Models {
+    public class AccountStatus {
+        public string accountStatus { get; set; }
+
+        public AccountStatus(string accountStatus) {
+            this.accountStatus = accountStatus;
+        }
+    }
+}
