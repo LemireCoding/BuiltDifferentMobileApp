4f7b7567a82c96c348f5a07815f14deb183ce74e
Chloedunwoody <chloe.dunwoody@hotmail.com>
2022-02-10 13:36:00 -0500

    CoachPaymentDTO to inlcude paypal link , a payment page for the client to pay their associated coach.

---
+++ BuiltDifferentMobileApp/BuiltDifferentMobileApp/Models/CoachPaymentDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuiltDifferentMobileApp.Models
+{
+    public class CoachPaymentDTO
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public bool isAvailable { get; set; }
+        public bool offersMeal { get; set; }
+        public bool offersWorkout { get; set; }
+        public int certificationId { get; set; }
+        public string gender { get; set; }
+        public bool isVerified { get; set; }
+        public string description { get; set; }
+        public double pricing { get; set; }
+        public string payPalLink { get; set; }
+    }
+}
