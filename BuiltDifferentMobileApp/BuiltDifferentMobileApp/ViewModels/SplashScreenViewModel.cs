0737b6b1c49ebe9df2abb31e656785457372be09
Brandon Thorng <brandonthorng@gmail.com>
2022-02-23 21:36:46 -0500

    Added splash screen

---
+++ BuiltDifferentMobileApp/BuiltDifferentMobileApp/ViewModels/SplashScreenViewModel.cs
@@ -0,0 +1,29 @@
+﻿using BuiltDifferentMobileApp.Services.NetworkServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuiltDifferentMobileApp.ViewModels {
+    public class SplashScreenViewModel : ViewModelBase {
+
+        public string Logo { get; set; }
+
+        private bool pageLoaded;
+        public bool PageLoaded {
+            get => pageLoaded;
+            set => SetProperty(ref pageLoaded, value);
+        }
+
+        public SplashScreenViewModel() {
+            Logo = APIConstants.GetLogo(false);
+            PageLoaded = false;
+
+            TriggerNextPage();
+        }
+
+        public async Task TriggerNextPage() {
+            PageLoaded = true;
+        }
+    }
+}
---
+++ BuiltDifferentMobileApp/BuiltDifferentMobileApp/Views/SplashScreen.xaml.cs
@@ -0,0 +1,28 @@
+﻿using BuiltDifferentMobileApp.Views.Login;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BuiltDifferentMobileApp.Views {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SplashScreen : ContentPage {
+
+        public SplashScreen() {
+            InitializeComponent();
+        }
+
+        private async void PageLoaded(object sender, ToggledEventArgs e) {
+            await Task.Delay(500);
+            await Logo.FadeTo(100, 1000);
+            await Task.Delay(500);
+            await Logo.FadeTo(0, 1000);
+            await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+        }
+    }
+}
